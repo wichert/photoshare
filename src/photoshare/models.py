90dca6870d3bc0e7f8b130214cc72f4163817563
Wichert Akkerman <wichert@wiggy.net>
2012-07-29 13:15:27 +0200

    Very basic datamodel

--- src/photoshare/models.py
+++ src/photoshare/models.py
@@ -1,28 +1,30 @@
-from sqlalchemy import (
-    Column,
-    Integer,
-    Text,
-    )
-
+from sqlalchemy import schema
+from sqlalchemy import types
+from sqlalchemy import orm
 from sqlalchemy.ext.declarative import declarative_base
-
-from sqlalchemy.orm import (
-    scoped_session,
-    sessionmaker,
-    )
-
+from sqlalchemy.ext.orderinglist import ordering_list
 from zope.sqlalchemy import ZopeTransactionExtension
 
-DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
+DBSession = orm.scoped_session(orm.sessionmaker(
+    extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
-class MyModel(Base):
-    __tablename__ = 'models'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text, unique=True)
-    value = Column(Integer)
 
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
+class User(Base):
+    __tablename__ = 'user'
+    id = schema.Column(types.Integer(), primary_key=True)
+    name = schema.Column(types.UnicodeText(), unique=True)
 
+
+class Photo(Base):
+    __tablename__ = 'photo'
+
+    id = schema.Column(types.Integer(), primary_key=True)
+    user_id = schema.Column(types.Integer(),
+            schema.ForeignKey('user.id', onupdate='CASCADE',
+                ondelete='CASCADE'))
+    position = schema.Column(types.Integer())
+    user = orm.relationship(User,
+            backref=orm.backref('photos',
+                collection_class=ordering_list('position'),
+                order_by=[position]))
--- src/photoshare/scripts/initializedb.py
+++ src/photoshare/scripts/initializedb.py
@@ -1,25 +1,20 @@
 import os
 import sys
 import transaction
-
 from sqlalchemy import engine_from_config
+from pyramid.paster import get_appsettings
+from pyramid.paster import setup_logging
+from ..models import DBSession
+from ..models import Base
+from ..models import User
 
-from pyramid.paster import (
-    get_appsettings,
-    setup_logging,
-    )
-
-from ..models import (
-    DBSession,
-    MyModel,
-    Base,
-    )
 
 def usage(argv):
     cmd = os.path.basename(argv[0])
     print('usage: %s <config_uri>\n'
           '(example: "%s development.ini")' % (cmd, cmd)) 
     sys.exit(1)
+
 
 def main(argv=sys.argv):
     if len(argv) != 2:
@@ -31,5 +26,16 @@ def main(argv=sys.argv):
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
     with transaction.manager:
-        model = MyModel(name='one', value=1)
-        DBSession.add(model)
+        DBSession.add(User(name=u'Åse'))
+        DBSession.add(User(name=u'Claude'))
+        DBSession.add(User(name=u'Erin'))
+        DBSession.add(User(name=u'Fleur'))
+        DBSession.add(User(name=u'Gustavo'))
+        DBSession.add(User(name=u'Helena'))
+        DBSession.add(User(name=u'Karl-Heinz'))
+        DBSession.add(User(name=u'Lando'))
+        DBSession.add(User(name=u'Linn'))
+        DBSession.add(User(name=u'Marjolein'))
+        DBSession.add(User(name=u'Panikos'))
+        DBSession.add(User(name=u'Rakel'))
+        DBSession.add(User(name=u'Wichert'))
